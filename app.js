be26502c26a519efb0412ac291be74b71e8ad515
Maribel <minicatsCB@users.noreply.github.com>
2019-02-27 20:33:51 +0100

    Add Firebase Authentication initialization

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ function initFirebase(){
   firebase.initializeApp(config);
 }
 
-M.Tabs.init(document.querySelector('.tabs'))
+function initFirebaseAuth() {
+    firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  if (user) {
+      console.log("User is signed in");
+  } else {
+      console.log("User is signed out");
+  }
+}
+
+M.Tabs.init(document.querySelector('.tabs'));
+
+initFirebase();
+initFirebaseAuth();
