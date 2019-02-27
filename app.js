a3ed8c26f9df62d486ebde20c9d4e6a9f26f5cda
Maribel <minicatsCB@users.noreply.github.com>
2019-02-27 20:27:24 +0100

    Add Firebase App initialization

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+function initFirebase(){
+  var config = {
+    apiKey: "yourApiKey",
+    authDomain: "yourAuthDomain",
+    projectId: "yourProjectId"
+  };
+  firebase.initializeApp(config);
+}
+
 M.Tabs.init(document.querySelector('.tabs'))
